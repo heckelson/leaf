ef569a44b8396e1b325f6f8ca1af86fe2c6c42d5
heckelson <26191361+heckelson@users.noreply.github.com>
2024-04-20 16:25:52 +0200

    add rough login input

--- plantmi-frontend/src/components/NavbarView.vue
+++ plantmi-frontend/src/components/NavbarView.vue
@@ -11,46 +11,54 @@
         </a>
       </div>
 
-      <div class="navbar-container" id="nav-elements">
+      <div class="navbar-container nav-elements">
         <a href="#">Map</a>
         <a href="#">Why?</a>
         <a href="#">Funding</a>
         <a href="#">About us</a>
         <a href="#">Impressum</a>
       </div>
+
+      <!--
+        TODO: Add v-if and v-else; if logged out show this, else show the username
+          and a logout field.
+      -->
+
+      <div class="navbar-container nav-elements">
+        <a href="#" @click="toggleLoginVisible()">Login</a>
+      </div>
     </div>
 
-    <!--<div class="login-section">
-            {% if 'username' in session %}
-            <p>
-                Logged in as user: {{ session['username'] }}
-            </p>
-            <p><a href="/auth/logout">Log out</a></p>
-            {% else %}
-            <div>
-                <form action="/auth/login" method="POST">
-                    <label>
-                        Username
-                        <input type="text" name="username" placeholder="Username">
-                    </label>
-                    <label>
-                        Passwort
-                        <input type="password" name="password" placeholder="Password">
-                    </label>
-
-                    <input type="submit" value="Log in">
-
-                </form>
-
-
-                {% endif %}
-            </div>
-
-        </div>-->
+    <div class="navbar-container" id="login-field" ref="login_field">
+      <form action="/login" class="navbar-container">
+        <div><label>Username</label><input type="text" /></div>
+        <div><label>Password</label><input type="password" /></div>
+        <div><button type="submit">Login</button></div>
+      </form>
+    </div>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data: function () {
+    return {
+      login_is_visible: true, // TODO: change to false once done with styling
+    };
+  },
+  methods: {
+    toggleLoginVisible: function () {
+      this.login_is_visible = !this.login_is_visible;
+
+      if (this.login_is_visible) {
+        this.$refs.login_field.style["display"] = "inherit";
+      } else {
+        this.$refs.login_field.style["display"] = "none";
+      }
+    },
+  },
+};
+</script>
 
 <style scoped>
 .logo_head {
@@ -60,6 +68,12 @@
   margin: 8px;
 }
 
+#header {
+  border-bottom: 2px solid #256eff;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 500;
+}
+
 .navbar {
   min-height: 6vh;
 
@@ -67,10 +81,6 @@
   flex-flow: row wrap;
   align-items: center;
   justify-content: space-between;
-
-  font-family: "Montserrat", sans-serif;
-  font-weight: 500;
-  border-bottom: 2px solid #256eff;
 }
 
 .navbar-container {
@@ -82,7 +92,7 @@
   justify-content: center;
 }
 
-#nav-elements a {
+.nav-elements a {
   color: var(--grandeis-blue);
   text-decoration: none;
 
@@ -91,9 +101,20 @@
   transition: all 150ms;
 }
 
-a:hover {
+.nav-elements a:hover {
   font-weight: 700;
-
   transform: translateY(-2px);
+}
+
+#login-field {
+  display: inherit;
+  /* TODO: change to 'none' once done with styling */
+
+  margin: 8px;
+  margin-top: 0;
+}
+
+#login-field input {
+  margin: 8px;
 }
 </style>
